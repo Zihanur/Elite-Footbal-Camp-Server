52589183238319f4c8ce42e08281c07c1d5a5fa6
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-09 01:24:46 +0600

    Get all class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,9 +12,7 @@ app.use(express.json());
 //eliteFootballCamp
 //UVs6X9JUBeq310l5
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +25,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const instructorCollection = client
+      .db("footballCampDB")
+      .collection("instructors");
+    const classCollection = client
+      .db("footballCampDB")
+      .collection("classes");
+
+    //instructors
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result);
+    });
+
+    //classes
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -35,7 +53,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
