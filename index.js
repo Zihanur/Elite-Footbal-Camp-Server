29b1f46c3fc7b7c76aca44e0cd1c249ed95960c4
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-08 17:46:37 +0600

    Secure mongoDB username and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,7 +13,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  `mongodb+srv://eliteFootballCamp:UVs6X9JUBeq310l5@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
