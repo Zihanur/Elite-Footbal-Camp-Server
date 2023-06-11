11458e4f91ac3902c95e93798c49e70bf6b164db
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-11 14:26:52 +0600

    Added student delete select class

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-//eliteFootballCamp
-//UVs6X9JUBeq310l5
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,9 +25,8 @@ async function run() {
     const instructorCollection = client
       .db("footballCampDB")
       .collection("instructors");
-    const classCollection = client
-      .db("footballCampDB")
-      .collection("classes");
+    const classCollection = client.db("footballCampDB").collection("classes");
+    const selectCollection = client.db("footballCampDB").collection("select");
 
     //instructors
     app.get("/instructors", async (req, res) => {
@@ -43,6 +39,33 @@ async function run() {
       const result = await classCollection.find().toArray();
       res.send(result);
     });
+
+    //student select class
+    app.post("/select", async (req, res) => {
+      const item = req.body;
+      const result = await selectCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.get("/select", async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await selectCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/select/:id", async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      const result = await selectCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
