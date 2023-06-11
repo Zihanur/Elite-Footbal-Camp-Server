3dcdbc39669bfed6bdfa3f30a4c7d559fb003aa1
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-11 15:44:01 +0600

    Added users post

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
       .collection("instructors");
     const classCollection = client.db("footballCampDB").collection("classes");
     const selectCollection = client.db("footballCampDB").collection("select");
+    const userCollection = client.db("footballCampDB").collection("users");
 
     //instructors
     app.get("/instructors", async (req, res) => {
@@ -60,11 +61,21 @@ async function run() {
 
     app.delete("/select/:id", async(req,res)=>{
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
-      console.log(query);
       const result = await selectCollection.deleteOne(query);
       res.send(result);
+    })
+
+    //all users
+    app.post('/users', async(req,res)=>{
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already Added' })
+      }
+      const result = userCollection.insertOne(user);
+      res.send(result)
     })
 
     // Connect the client to the server	(optional starting in v4.7)
