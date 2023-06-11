e3e61c882c8f3392edc10b18dec0286f09433a05
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-11 16:24:01 +0600

    Added all users get

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users',async(req,res)=>{
+      const result= await userCollection.find().toArray();
+      res.send(result);
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
